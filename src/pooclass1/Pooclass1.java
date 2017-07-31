5d19c9c703ed710f2be5cd1d035370d8f9a905f2
Estudiante <Estudiante@10.203.2.157>
2017-07-31 14:59:17 -0500

    el ejemplo hola mundo

---
+++ src/pooclass1/Pooclass1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pooclass1;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Pooclass1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+        int x=9;
+        for(int i =0; i<x; i++){
+            System.out.println("hola mundo"+(i+1));
+        }
+        // TODO code application logic here
+    }
+    
+}
